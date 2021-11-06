9f428e86a843636c7f390315f160ea2ea753fb9a
Vlada Plutalova <vladaplutalova@yandex.ru>
2021-11-06 11:59:17 +0300

    feat: add task 01

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+
+const wayOfFile = path.join(__dirname, 'text.txt')
+const readableFile = fs.createReadStream(wayOfFile,'utf-8');
+readableFile.on('data', chunk => stdout.write(chunk));
