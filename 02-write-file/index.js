a11f58906e39068bb6a0456fd59cddbc4e95ec60
Vlada Plutalova <vladaplutalova@yandex.ru>
2021-11-09 01:30:08 +0300

    fix: corrected 02 task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -19,12 +19,24 @@ const handler2 = () =>
   stdin.on("data", (data) => {
     // функция,которая будет записывать текст в файл
     writeableStream.write(data); // Запись данных - метод write(), в который передаются данные
-    writeableStream.end(""); // Завершение записи
+    //writeableStream.end(""); // Завершение записи
     readableStream.pipe(writeableStream); // У потока чтения вызывается метод pipe(), в который передается поток для записи
+
+    process.on("SIGINT", () => {
+      console.log("Удачи в изучении Node.js!");
+    });
+
+    function handle() {
+      console.log("Пока!");
+      process.exit(-1);
+    }
+
+    process.on("SIGINT", handle);
   });
-process.on("exit", () => stdout.write("Удачи в изучении Node.js!"));
+
+process.on("exit", () => stdout.write("Хорошего дня!"));
 
 emitter.on("start", handler1); // первый обработчик запускается при событии старт
-emitter.on("start", handler2); // второй нужно запустить на событии старт
+emitter.once("start", handler2); // второй нужно запустить на событии старт
 
 emitter.emit("start"); // выводит 1, затем 2 (генерируем событие, чтобы сработал его обработчик->метод emit())
